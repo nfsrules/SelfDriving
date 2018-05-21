fd11fb936b6907da653a3325bacaa709d6c7e2aa
Nelson Fernandez <nelson.fernandez@axionable.com>
2018-05-21 16:09:54 +0200

    updated scripts

--- architectures.py
+++ architectures.py
@@ -8,18 +8,65 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, 
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense
 from keras.layers import Flatten, BatchNormalization, Activation, MaxPool2D
 from keras.layers import Conv1D
 from keras import backend as K
 
 
+def create_iNet():
+    '''Proposed modifications to Nvidia PilotNet model. 
+
+    '''
+    model = Sequential()
+    model.add(Conv2D(64, 3, data_format='channels_last', kernel_initializer='he_normal',
+                     input_shape=(160, 320, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(24, (5, 5), strides=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(36, (5, 5), strides=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(48, (5, 5), strides=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+
+    model.add(Dropout(0.6))
+    
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('elu'))
+    model.add(Dropout(0.6))
+
+    model.add(Dense(1))
+
+    model.summary()
+
+    return model
+
+
 def build_model(input_shape=(160,320,3), dropout=0.6):
     """
-    Modified NVIDIA model
+   Classic NVIDIA model.
+
     """
     model = Sequential()
-    #model.add(Lambda(lambda x: x/127.5-1.0, input_shape=input_shape))
+    
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=input_shape))
     model.add(Conv2D(24, (5, 5), activation='elu', strides=(2, 2), input_shape=input_shape))
     model.add(BatchNormalization())
 
@@ -31,7 +78,6 @@ def build_model(input_shape=(160,320,3), dropout=0.6):
     
     model.add(Conv2D(64, (3, 3), activation='elu'))
     model.add(BatchNormalization())
-
 
     model.add(Conv2D(64, (3, 3), activation='elu'))
     model.add(BatchNormalization())
@@ -47,12 +93,13 @@ def build_model(input_shape=(160,320,3), dropout=0.6):
 
     return model
 
+
 def build_model2(input_shape=(160,320,3), dropout=0.6):
     """
-    Modified NVIDIA model
+    Modified NVIDIA model. Conv1D classificator.
+
     """
     model = Sequential()
-    #model.add(Lambda(lambda x: x/127.5-1.0, input_shape=input_shape))
     model.add(Conv2D(24, (5, 5), strides=(2, 2), input_shape=input_shape))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
@@ -92,55 +139,7 @@ def build_model2(input_shape=(160,320,3), dropout=0.6):
     model.add(BatchNormalization())
     model.add(Activation('elu'))
     
-    #model.add(Dense(100, activation='elu'))
-    #model.add(Dense(50, activation='elu'))
-    #model.add(Dense(10, activation='elu'))
     model.add(Dense(1))
-    model.summary()
-
-    return model
-
-
-def create_iNet():
-    model = Sequential()
-    model.add(Conv2D(64, 3, data_format='channels_last', kernel_initializer='he_normal',
-                     input_shape=(160, 320, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-
-    model.add(Conv2D(24, (5, 5), strides=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-    #model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-    #model.add(Dropout(0.6))
-
-    model.add(Conv2D(36, (5, 5), strides=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-
-    model.add(Conv2D(48, (5, 5), strides=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-    #model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-    
-    model.add(Conv2D(64, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-    #model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-    model.add(Dropout(0.6))
-    
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(BatchNormalization())
-    model.add(Activation('elu'))
-    model.add(Dropout(0.6))
-
-    model.add(Dense(1))
-    #model.add(Activation('tanh'))
     model.summary()
 
     return model
--- training.py
+++ training.py
@@ -4,23 +4,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, 
-from keras.layers import Flatten, BatchNormalization, Activation, MaxPool2D
-from keras.layers import Conv1D
 from keras import backend as K
-
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
 
 from architectures import *
 
-# Imports for visualisation
-#import matplotlib.pyplot as plt
-#%matplotlib inline
-#%config InlineBackend.figure_format = 'retina'
-#plt.style.use('ggplot')
 
 #Load data
 X = np.load('Dataset/train_X.npy')
@@ -31,11 +19,10 @@ print('Training Y shape =', np.shape(Y))
 
 
 # Create model
-model = build_model() #create_iNet() # build_model2()
-model.summary()
+model = create_iNet()
 
 
-# Configure optimizer and loss function
+# Custon RMSE loss
 #def root_mean_squared_error(y_true, y_pred):
 #        return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1)) 
     
@@ -43,6 +30,7 @@ model.compile(optimizer='rmsprop',
               loss='mse',
               metrics =["accuracy"]
              )
+
 
 #checkpointer = ModelCheckpoint(filepath='autopilot_checkpoint_mse.h5', verbose=1, save_best_only=True)
 
@@ -56,7 +44,9 @@ history = model.fit(X, Y,
                     batch_size=100, 
                     epochs=30, 
                     validation_split=0.15,
-                    callbacks=[reduce_LR, early_stopping_monitor])
+                    callbacks=[reduce_LR, early_stopping_monitor],
+                    shuffle=True)
 
 
 model.save('autopilot_nvidia.hdf5')
+
